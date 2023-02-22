00c45f1b124d09bae6119e299c441949064890e7
Abilay10101 <60212530+Abilay10101@users.noreply.github.com>
2023-02-22 14:27:52 +0600

    Added extension to date and Note class

---
+++ NinjaNotes/Date+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Date+Extension.swift
+//  NinjaNotes
+//
+//  Created by Arip Khozhbanov on 22.02.2023.
+//
+
+import Foundation
+
+extension Date {
+    func format() -> String {
+        let formatter = DateFormatter()
+        if Calendar.current.isDateInToday(self) {
+            formatter.dateFormat = "h:mm a"
+            
+        } else {
+            formatter.dateFormat = "dd/MM/yy"
+        }
+        return formatter.string(from: self)
+    }
+}
---
+++ NinjaNotes/Note.swift
@@ -0,0 +1,24 @@
+//
+//  Note.swift
+//  NinjaNotes
+//
+//  Created by Arip Khozhbanov on 22.02.2023.
+//
+
+import Foundation
+
+class Note {
+    let id = UUID()
+    var text: String = ""
+    var lastUpdated: Date = Date()
+    
+    var title: String {
+        return text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines).first ?? "" // returns the first line of the text
+    }
+    
+    var desc: String {
+        var lines = text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: .newlines)
+        lines.removeFirst()
+        return "\(lastUpdated.format()) \(lines.first ?? "")" // return second line
+    }
+}
